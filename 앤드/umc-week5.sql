ebf680085df2daa76c4009e1e2a077bdb0ac41df
ssongmina <thdalsdk5498@naver.com>
2024-05-08 21:59:40 +0900

    [CHORE] 4,5주차 수정

--- 앤드/umc-week5.sql
+++ 앤드/umc-week5.sql
@@ -1,11 +1,11 @@
 //내 id가 1인 경우
 select m.point, s.name
-from (member_mission as my_mission natural join mission as m using region_id) join store as s
+from (member_mission as my_mission join mission as m on m.id = my_mission.mission_id) join store as s on s.id = m.store_id
 where my_mission.status = ‘진행 완료’ and my_mission.member_id = 1 
 limit 10;
 
 select m.point, s.name
-from (member_mission as my_mission natural join mission as m using region_id) join store as s
+from (member_mission as my_mission join mission as m on m.id = my_mission.mission_id) join store as s on s.id = m.store_id
 where my_mission.status = ‘진행 중’ and my_mission.member_id = 1 
 limit 10;
 
@@ -14,10 +14,9 @@ limit 10;
 insert into review(member_id, store_id, score, body, created_at) values (1,2,5,’음 너무 맛있어요…’,CURRENT_TIMESTAMP);
 
 
-// 내 id가 1인 경우
 select s.name, m.deadline, m.point
-from (region as r join store as s on s.region_id == r.id) join mission as m using region_id
-where r.name = ‘안암동’ 
+from store as s join mission as m 
+where s.name = ‘안암동’ 
 limit 10;
 
 // 내 id가 1인 경우
