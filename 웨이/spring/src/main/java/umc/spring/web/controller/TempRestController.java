52c590c4b7c008e876104273a0c4d5fd91687ba7
thwldus <gee906@naver.com>
2024-05-27 17:48:54 +0900

    [FEAT] API 응답 통일

---
+++ 웨이/spring/src/main/java/umc/spring/web/controller/TempRestController.java
@@ -0,0 +1,19 @@
+package umc.spring.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import umc.spring.apiPayload.ApiResponse;
+import umc.spring.converter.TempConverter;
+import umc.spring.web.dto.TempResponse;
+
+@RestController
+@RequestMapping("/temp")
+@RequiredArgsConstructor
+public class TempRestController {
+
+    @GetMapping("/test")
+    public ApiResponse<TempResponse.TempTestDTO> testAPI(){
+
+        return ApiResponse.onSuccess(TempConverter.toTempTestDTO());
+    }
+}
