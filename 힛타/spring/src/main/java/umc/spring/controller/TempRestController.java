df7b6dd558ec2e87c424af077c35ccbe1064c0ba
beom-i <lybgreen77@gmail.com>
2024-05-30 10:04:55 +0900

    [FEAT] API 응답 통일, 에러 핸들러 추가

---
+++ 힛타/spring/src/main/java/umc/spring/controller/TempRestController.java
@@ -0,0 +1,32 @@
+package umc.spring.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import umc.spring.apiPayload.ApiResponse;
+import umc.spring.converter.TempConverter;
+import umc.spring.dto.TempResponse;
+import umc.spring.service.TempQueryService;
+
+@RestController
+@RequestMapping("/temp")
+@RequiredArgsConstructor
+public class TempRestController {
+
+    private final TempQueryService tempQueryService;
+
+    @GetMapping("/test")
+    public ApiResponse<TempResponse.TempTestDTO> testAPI(){
+
+        return ApiResponse.onSuccess(TempConverter.toTempTestDTO());
+    }
+
+    @GetMapping("/exception")
+    public ApiResponse<TempResponse.TempExceptionDTO> exceptionAPI(@RequestParam("flag") Integer flag){
+        tempQueryService.CheckFlag(flag);
+        return ApiResponse.onSuccess(TempConverter.toTempExceptionDTO(flag));
+    }
+}
---
+++ 힛타/spring/src/main/java/umc/spring/service/TempCommandServiceImpl.java
@@ -0,0 +1,10 @@
+package umc.spring.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TempCommandServiceImpl implements TempCommandService{
+
+}
